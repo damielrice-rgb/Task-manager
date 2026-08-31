94d3551ba0d66f998a2859a277bdfd4b5eb0314b
Damiel Rice <damielrice@gmail.com>
2026-08-31 13:09:25 -0500

    Add controlled title input

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -1,9 +1,18 @@
 import type { TaskFormData } from '../../types/';
+import { useState } from 'react';
 
 export const TaskForm = () => {
+  const [title, setTitle] = useState('');
   return (
     <div>
       <h2>Add New Task</h2>
+
+      <label htmlFor="title">Title</label>
+      <input
+        id="title"
+        type="text"
+        value={title}
+        onChange={(event) => setTitle(event.target.value) }/>
     </div>
   );
 };
